9a823ea8d35d5f8e5f8d90a476de4080c162947d
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-24 13:21:37 +0100

    create ACR

--- Azure AKS Deployment Pipeline/main.tf
+++ Azure AKS Deployment Pipeline/main.tf
@@ -1,5 +1,15 @@
-#create resource group
+# create resource group
 resource "azurerm_resource_group" "infra_rg_123" {
   name     = "infra_rg_123"
   location = "West Europe"
 }
+
+# create ACR
+resource "azurerm_container_registry" "acr" {
+  name                = "acrapp01"
+  resource_group_name = azurerm_resource_group.infra_rg_123.name
+  location            = azurerm_resource_group.infra_rg_123.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
