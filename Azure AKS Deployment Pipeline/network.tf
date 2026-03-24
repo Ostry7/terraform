98cfa65a03e11545d19f7c1932c5581244bf2793
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-24 14:25:15 +0100

    add vnet_subnet_id in default_node_pool

--- Azure AKS Deployment Pipeline/network.tf
+++ Azure AKS Deployment Pipeline/network.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "aks_subnet" {
   name = "aks-subnet"
   resource_group_name = azurerm_resource_group.infra_rg_123.name
-  virtual_network_name = azurerm_virtual_network.vnet
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes = ["10.240.0.0/16"]
   
 }
