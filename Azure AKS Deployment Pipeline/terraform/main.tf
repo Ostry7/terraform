1fd0db90a16b1ba96a9172aaa8850c5fe126d44b
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-25 16:10:33 +0100

    test commit for feature branch

--- Azure AKS Deployment Pipeline/terraform/main.tf
+++ Azure AKS Deployment Pipeline/terraform/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_container_registry" "acr" {
 
 # create AKS
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
+  name                = "example-aks1-test"
   location            = azurerm_resource_group.infra_rg_123.location
   resource_group_name = azurerm_resource_group.infra_rg_123.name
   dns_prefix          = "exampleaks1"
