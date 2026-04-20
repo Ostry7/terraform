44ff602e1afeb5dde14a0b25ffd7da8aeca52117
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-04-20 17:35:34 +0200

    create AKS resource

--- GitOps-Based Kubernetes Platform on Azure/terraform/main.tf
+++ GitOps-Based Kubernetes Platform on Azure/terraform/main.tf
@@ -6,9 +6,38 @@ resource "azurerm_resource_group" "gitops_rg2345234" {
 
 # create ACR
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "gitopscontainerregistry7677"
   resource_group_name = azurerm_resource_group.gitops_rg2345234.name
   location            = azurerm_resource_group.gitops_rg2345234.location
   sku                 = "Basic"
   admin_enabled       = false
+}
+
+# create AKS
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1_test234"
+  location            = azurerm_resource_group.gitops_rg2345234.location
+  resource_group_name = azurerm_resource_group.gitops_rg2345234.name
+  dns_prefix          = "exampleaks1"
+  oidc_issuer_enabled = true 
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2ls_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.0.0.10"
+    service_cidr   = "10.0.0.0/16"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
