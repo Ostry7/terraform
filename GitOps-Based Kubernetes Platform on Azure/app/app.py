eb05429646cd2f93eeb6dd6456445e20ae395331
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-05-13 14:13:08 +0200

    add requests counter

--- GitOps-Based Kubernetes Platform on Azure/app/app.py
+++ GitOps-Based Kubernetes Platform on Azure/app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter
 import random
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app) 
+
+fact_counter = Counter('fact_requests_total', 'Total number of /fact requests')
+health_counter = Counter('health_requests_total', 'Total number of /health requests')
 
 facts = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum imperdiet diam purus, eu varius velit porttitor non. Vestibulum ornare blandit ex, ut convallis velit interdum ut. Fusce augue justo, dignissim non mi ut, fermentum maximus dolor. Phasellus purus orci, fermentum eu tincidunt eget, eleifend nec dolor. Nam lobortis mauris tortor, in accumsan neque porta auctor. Integer elementum ornare eros, in consequat velit interdum vel. Duis euismod augue molestie ex dictum, in facilisis tortor pellentesque. Nam sagittis eu dui eget eleifend. Morbi tempus leo lacus, eu molestie magna semper sed. Interdum et malesuada fames ac ante ipsum primis in faucibus.  Sed nec elit ligula. Pellentesque pretium urna eu posuere sollicitudin. Vestibulum id maximus diam. Curabitur mollis nisl metus, in vestibulum dui volutpat vitae. Cras     consectetur laoreet semper. Nam eget eleifend diam, non aliquet purus. Quisque enim risus, faucibus ut nisi ac, fermentum vehicula est. Integer vitae tristique felis. Integer elementum ac risus eu dapibus. Fusce dignissim ex leo, eu gravida metus auctor sed."
@@ -15,6 +19,7 @@ def home():
 
 @app.route("/health")
 def health():
+    health_counter.inc() 
     return jsonify({"status": "healthy"})
 
 @app.route("/fact")
