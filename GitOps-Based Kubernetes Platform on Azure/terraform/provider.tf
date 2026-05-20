6f80d1a169e7b4c18a0360509eb23b5fc4ef10ba
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-05-20 14:49:43 +0200

    change storage_account_name as there is a new az subscription

--- GitOps-Based Kubernetes Platform on Azure/terraform/provider.tf
+++ GitOps-Based Kubernetes Platform on Azure/terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate34563456"
+    storage_account_name = "tfstate565656456"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
