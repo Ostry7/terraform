3d8c8914f271326d359462848b67ecb2965ec7aa
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-06-09 12:55:53 +0200

    increate node_count

--- GitOps-Based Kubernetes Platform on Azure/terraform/main.tf
+++ GitOps-Based Kubernetes Platform on Azure/terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name           = "default"
-    node_count     = 1
+    node_count     = 2
     vm_size        = "Standard_B2ls_v2"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
