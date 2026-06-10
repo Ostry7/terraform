06da7431e01166ab90c806e7eb8bf6ff7fa60988
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-06-10 13:57:15 +0200

    change service CIDR

--- GitOps-Based Kubernetes Platform on Azure/terraform/dr-site.tf
+++ GitOps-Based Kubernetes Platform on Azure/terraform/dr-site.tf
@@ -36,8 +36,8 @@ resource "azurerm_kubernetes_cluster" "dr" {
 
   network_profile {
     network_plugin = "azure"
-    dns_service_ip = "10.1.0.10"
-    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.2.0.10"
+    service_cidr   = "10.2.0.0/16"
   }
 
   identity {
